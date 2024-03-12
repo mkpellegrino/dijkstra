d7757b6ce4db58daef9f247768fbe73d06d1bf83
Mike Pellegrino <47326270+mkpellegrino@users.noreply.github.com>
2024-03-12 16:03:15 -0400

    Added Territories and Randomness for Troops

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -13,6 +13,61 @@
 #include <random>
 
 
+// NORTH AMERICA
+#define ALASKA 0
+#define NORTHWEST_TERRITORY 1
+#define ALBERTA 2
+#define GREENLAND 3
+#define ONTARIO 4
+#define QUEBEC 5
+#define WESTERN_US 6
+#define EASTERN_US 7
+#define CENTRAL_AMERICA 8
+#define ICELAND 9
+
+// SOUTH AMERICA
+#define VENEZUELA 10
+#define PERU 11
+#define ARGENTINA 12
+#define BRAZIL 13
+
+// EUROPE
+#define GREAT_BRITAIN 14
+#define SCANDANAVIA 15
+#define NORTHERN_EUROPE 16
+#define WESTERN_EUROPE 17
+#define SOUTHERN_EUROPE 18
+#define UKRAINE 19
+
+// AFRICA
+#define NORTH_AFRICA 20
+#define EAST_AFRICA 21
+#define EGYPT 22
+#define CONGO 23
+#define SOUTH_AFRICA 24
+#define MADAGASCAR 25
+
+// AUSTRALIA
+#define NEW_GUINEA 26
+#define INDONESIA 27
+#define WESTERN_AUSTRALIA 28
+#define EASTERN_AUSTRALIA 29
+
+// ASIA
+#define URAL 30
+#define SIBERIA 31
+#define YAKUTSK 32
+#define IRKUTSK 33
+#define JAPAN 34
+#define MONGOLIA 35
+#define AFGHANISTAN 36
+#define THE_MIDDLE_EAST 37
+#define INDIA 38
+#define CHINA 39
+#define SIAM 40
+#define KAMCHATKA 41
+
+
 unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
 std::default_random_engine e(seed);
 
@@ -212,16 +267,6 @@ void deleteAllNodes()
 
 
 
-// ostream& operator <<(ostream& out, const Path& p)
-// {  
-//   for( int i=0;i<p.getSize();i++ )
-//     {
-//       out << p.v[i] << " ";
-//     }
-//   return out;
-// }
-  
-
 /*
   To know how Dijkstra's algorithm works behind the scene, look at the below steps to understand it in detail:
 
@@ -249,7 +294,6 @@ public:
 private:
   Node * node;
   int weight;
-  int id;  
 };
 
 
@@ -270,20 +314,6 @@ vector<Triplet*> visited_vector;
 vector<Node*> path;
 vector<Node*> lineage;
 
-
-int find( Node* n )
-{
-  int return_value = -1;
-  for( vector<Triplet*>::iterator j=visited_vector.begin(); j<visited_vector.end(); j++ )
-    {
-      Triplet * t = visited_vector[distance(visited_vector.begin(), j)];
-      if( t->getNode() == n )
-	{
-	  return_value = distance(visited_vector.begin(), j);
-	}
-    }
-  return return_value;
-}
 
 bool alreadyVisited( Node* n )
 {
@@ -310,7 +340,7 @@ bool isInLineage( Node* n )
 void dump(vector<Node*> v)
 {
   for( int i=0;i<v.size();i++)
-    cout << *v[i] << " ";
+    cout << "[" << *v[i] << "] ";
   cout << endl;
 }
 
@@ -374,13 +404,10 @@ void getEasiestPath( Node* start, Node* end, int weight_total=0)
 #endif
       visited_vector.push_back( t );
     }
-
-
   lineage.push_back( start );
 #ifdef DEBUG
   dump( lineage );
 #endif
-
   vector<Node*> l = start->getLinks();
   weight_total += start->getTroops();
   
@@ -399,7 +426,6 @@ void getEasiestPath( Node* start, Node* end, int weight_total=0)
 	{
 	  path.push_back( lineage[i] );
 	}
-      
       goal_count++;
     }
 #ifdef DEBUG
@@ -416,11 +442,8 @@ void getEasiestPath( Node* start, Node* end, int weight_total=0)
 	    {
 	      getEasiestPath( l[j], end, weight_total );
 	    }
-	  
-	  
 	}
     }
-
   lineage.pop_back();	      
   return;
 }
@@ -438,22 +461,6 @@ void setWeight( int i, int j )
   nodes_vector[i]->setTroops( j );
 }
 
-Node * getBacktrackNode( Node * n )
-{
-  vector<Node*> l = n->getLinks();
-  int which_is_closer_to_home;
-  
-  int min=getFromVisitedVector(l[0])->getWeight()+1;
-
-  for( int i=0; i < l.size(); i++ )
-    {
-      if( getFromVisitedVector(l[i])->getWeight() < min )
-	{
-	  which_is_closer_to_home = i;
-	}
-    }
-  return getFromVisitedVector(l[which_is_closer_to_home])->getNode();  
-}
 
 int main()
 {
@@ -463,96 +470,301 @@ int main()
 #ifdef DEBUG
   cerr << "populating" << endl;
 #endif
-  nodes_vector.push_back( new Node( 5, 5, new string( "A" ) ) );   // 0
-  nodes_vector.push_back( new Node( 10, 4, new string( "B" ) ) );  // 1
-  nodes_vector.push_back( new Node( 15, 5, new string( "C" ) ) );  // 2
-  nodes_vector.push_back( new Node( 20, 4, new string( "D" ) ) );  // 3
-  nodes_vector.push_back( new Node( 5, 2, new string( "G" ) ) );   // 4
-  nodes_vector.push_back( new Node( 10, 1, new string( "H" ) ) );  // 5
-  nodes_vector.push_back( new Node( 15, 3, new string( "E" ) ) );  // 6
-  nodes_vector.push_back( new Node( 20, 0, new string( "J" ) ) );  // 7
-  nodes_vector.push_back( new Node( 15, 0, new string( "I" ) ) );  // 8
-  nodes_vector.push_back( new Node( 0, 0, new string( "F" ) ) );   // 9 
-  nodes_vector.push_back( new Node( 18, -2, new string( "K" ) ) ); // 10
-  nodes_vector.push_back( new Node( 20, -2, new string( "L" ) ) ); // 11
-  nodes_vector.push_back( new Node( 20, -5, new string( "M" ) ) ); // 12
+  nodes_vector.push_back( new Node( 1, 0, new string( "Alaska" ) ) );   // 0
+  nodes_vector.push_back( new Node( 1, 1, new string( "Northwest Territory" ) ) );  // 1
+  nodes_vector.push_back( new Node( 2, 0, new string( "Alberta" ) ) );  // 2
+  nodes_vector.push_back( new Node( 1, 2, new string( "Greenland" ) ) );  // 3
+  nodes_vector.push_back( new Node( 2, 1, new string( "Ontario" ) ) );   // 4
+  nodes_vector.push_back( new Node( 2, 2, new string( "Quebec" ) ) );  // 5
+  nodes_vector.push_back( new Node( 3, 0, new string( "Western U.S." ) ) );  // 6
+  nodes_vector.push_back( new Node( 3, 1, new string( "Eastern U.S." ) ) );  // 7
+  nodes_vector.push_back( new Node( 4, 1, new string( "Central America" ) ) );  // 8
+  nodes_vector.push_back( new Node( 1, 3, new string( "Iceland" ) ) );   // 9
   
-  nodes_vector.push_back( new Node( 22, -4, new string( "N" ) ) ); // 13
-  nodes_vector.push_back( new Node( 22, -7, new string( "O" ) ) ); // 14
+  nodes_vector.push_back( new Node( 4, 2, new string( "Venezuela" ) ) ); // 10
+  nodes_vector.push_back( new Node( 5, 2, new string( "Peru" ) ) ); // 11
+  nodes_vector.push_back( new Node( 5, 3, new string( "Argentina" ) ) ); // 12
+  nodes_vector.push_back( new Node( 4, 3, new string( "Brazil" ) ) ); // 13
 
   
+  nodes_vector.push_back( new Node( 2, 3, new string( "Great Britain" ) ) ); 
+  nodes_vector.push_back( new Node( 1, 4, new string( "Scandanavia" ) ) ); 
+  nodes_vector.push_back( new Node( 2, 4, new string( "N. Europe" ) ) ); 
+  nodes_vector.push_back( new Node( 3, 4, new string( "W. Europe" ) ) ); 
+  nodes_vector.push_back( new Node( 2, 5, new string( "S. Europe" ) ) ); 
+  nodes_vector.push_back( new Node( 1, 5, new string( "Ukraine" ) ) ); 
+
+  nodes_vector.push_back( new Node( 3, 5, new string( "N. Africa" ) ) ); 
+  nodes_vector.push_back( new Node( 4, 6, new string( "E. Africa" ) ) ); 
+    nodes_vector.push_back( new Node( 3, 6, new string( "Egypt" ) ) ); 
+  nodes_vector.push_back( new Node( 4, 5, new string( "Congo" ) ) ); 
+  nodes_vector.push_back( new Node( 5, 5, new string( "S. Africa" ) ) ); 
+  nodes_vector.push_back( new Node( 5, 6, new string( "Madagascar" ) ) ); 
+
+    nodes_vector.push_back( new Node( 4, 9, new string( "New Guinea" ) ) ); 
+  nodes_vector.push_back( new Node( 4, 8, new string( "Indonesia" ) ) ); 
+  nodes_vector.push_back( new Node( 5, 8, new string( "W Australia" ) ) ); 
+  nodes_vector.push_back( new Node( 5, 9, new string( "E Australia" ) ) ); 
+
+  nodes_vector.push_back( new Node( 0, 6, new string( "Ural" ) ) ); 
+  nodes_vector.push_back( new Node( 0, 7, new string( "Siberia" ) ) ); 
+  nodes_vector.push_back( new Node( 0, 8, new string( "Yakutsk" ) ) ); 
+  nodes_vector.push_back( new Node( 1, 8, new string( "Irkutsk" ) ) ); 
+  nodes_vector.push_back( new Node( 1, 9, new string( "Japan" ) ) ); 
+  nodes_vector.push_back( new Node( 2, 8, new string( "Mongolia" ) ) ); 
+  nodes_vector.push_back( new Node( 1, 6, new string( "Afghan" ) ) ); 
+  nodes_vector.push_back( new Node( 2, 6, new string( "The Middle East" ) ) ); 
+  nodes_vector.push_back( new Node( 2, 7, new string( "India" ) ) ); 
+  nodes_vector.push_back( new Node( 1, 7, new string( "China" ) ) ); 
+  nodes_vector.push_back( new Node( 3, 7, new string( "Siam" ) ) ); 
+    nodes_vector.push_back( new Node( 0, 9, new string( "Kamchatka" ) ) ); 
+
 
 #ifdef DEBUG
   cerr << "linking" << endl;
 #endif
-  link( 0, 1 );
-  link( 0, 4 );
+  link( ALASKA, NORTHWEST_TERRITORY );
+  link( ALASKA, ALBERTA );
+  link( ALASKA, KAMCHATKA );
+
   
-  link( 1, 6 );
-  link( 1, 4 );
 
-  link( 2, 5 );
-  link( 2, 6 );
 
-  link( 3, 7 );
-  link( 3, 6 );
 
-  link( 4, 5 );
-  link( 4, 9 );
+  link( NORTHWEST_TERRITORY, ALASKA );
+  link( NORTHWEST_TERRITORY, ALBERTA );
+  link( NORTHWEST_TERRITORY, ONTARIO );
+  link( NORTHWEST_TERRITORY, GREENLAND );
 
-  link( 5, 6 );
-  link( 5, 8 );
+  link( ALBERTA, ALASKA );
+  link( ALBERTA, NORTHWEST_TERRITORY );
+  link( ALBERTA, ONTARIO );
+  link( ALBERTA, WESTERN_US );
+  
+  link( GREENLAND, NORTHWEST_TERRITORY );
+  link( GREENLAND, ONTARIO );
+  link( GREENLAND, QUEBEC );
+  link( GREENLAND, ICELAND );
 
-  link( 6, 7 );
-  link( 6, 10 );
+  link( ONTARIO, NORTHWEST_TERRITORY );
+  link( ONTARIO, GREENLAND );
+  link( ONTARIO, QUEBEC );
+  link( ONTARIO, EASTERN_US );
+  link( ONTARIO, WESTERN_US );
+  link( ONTARIO, ALBERTA );
 
-  link( 7, 8 );
-  link( 7, 11 );
+  link( QUEBEC, EASTERN_US );
+  link( QUEBEC, ONTARIO );
+  link( QUEBEC, GREENLAND );
 
-  link( 8, 11 );
+  link( WESTERN_US, ALBERTA );
+  link( WESTERN_US, ONTARIO );
+  link( WESTERN_US, EASTERN_US );
+  link( WESTERN_US, CENTRAL_AMERICA );
+  
+  link( EASTERN_US, WESTERN_US );
+  link( EASTERN_US, CENTRAL_AMERICA );
+  link( EASTERN_US, ONTARIO );
+  link( EASTERN_US, QUEBEC );
 
-  link( 10, 11 );
-  link( 10, 12 );
+  link( CENTRAL_AMERICA, WESTERN_US );
+  link( CENTRAL_AMERICA, EASTERN_US );
+  link( CENTRAL_AMERICA, VENEZUELA );
 
-  link( 11, 12 );
-  link( 10, 13 );
-  link( 11, 13 );
-  link( 12, 13 );
-  link( 14, 13 );
+  link( VENEZUELA, CENTRAL_AMERICA );
+  link( VENEZUELA, BRAZIL );
+  link( VENEZUELA, PERU );
+
+  link( BRAZIL, VENEZUELA );
+  link( BRAZIL, PERU );
+  link( BRAZIL, ARGENTINA );
+  link( BRAZIL, NORTH_AFRICA );
+
+  link( PERU, VENEZUELA );
+  link( PERU, BRAZIL );
+  link( PERU, ARGENTINA );
+
+  link( ARGENTINA, PERU );
+  link( ARGENTINA, BRAZIL );
+  
+  link( ICELAND, GREAT_BRITAIN );
+  link( ICELAND, GREENLAND );
+  link( ICELAND, SCANDANAVIA );
+
+  link( KAMCHATKA, ALASKA );
+  link( KAMCHATKA, JAPAN );
+  link( KAMCHATKA, MONGOLIA );
+  link( KAMCHATKA, IRKUTSK );
+  link( KAMCHATKA, YAKUTSK );
+
+  link( JAPAN, MONGOLIA );
+  link( JAPAN, KAMCHATKA );
+
+
+  link( AFGHANISTAN, UKRAINE );
+  link( AFGHANISTAN, URAL );
+  link( AFGHANISTAN, CHINA );
+  link( AFGHANISTAN, INDIA );
+  link( AFGHANISTAN, THE_MIDDLE_EAST );
+ 
+  
+  link( CHINA, SIAM );
+  link( CHINA, INDIA );
+  link( CHINA, AFGHANISTAN );
+  link( CHINA, URAL );
+  link( CHINA, SIBERIA );
+  link( CHINA, MONGOLIA );
+
+  link( MONGOLIA, JAPAN );
+  link( MONGOLIA, CHINA );
+  link( MONGOLIA, SIBERIA );
+  link( MONGOLIA, IRKUTSK );
+  link( MONGOLIA, KAMCHATKA );
+
+  link( IRKUTSK, MONGOLIA );
+  link( IRKUTSK, KAMCHATKA );
+  link( IRKUTSK, YAKUTSK );
+  link( IRKUTSK, SIBERIA );
+
+  link( YAKUTSK, KAMCHATKA );
+  link( YAKUTSK, SIBERIA );
+  link( YAKUTSK, IRKUTSK );
+
+  link( SIBERIA, URAL );
+  link( SIBERIA, CHINA );
+  link( SIBERIA, MONGOLIA );
+  link( SIBERIA, IRKUTSK );
+  link( SIBERIA, YAKUTSK );
+
+  link( INDIA, THE_MIDDLE_EAST );
+  link( INDIA, AFGHANISTAN );
+  link( INDIA, CHINA );
+  link( INDIA, SIAM );
+
+  link( THE_MIDDLE_EAST, EGYPT );
+  link( THE_MIDDLE_EAST, SOUTHERN_EUROPE );
+  link( THE_MIDDLE_EAST, UKRAINE );
+  link( THE_MIDDLE_EAST, AFGHANISTAN );
+  link( THE_MIDDLE_EAST, INDIA );
+  link( THE_MIDDLE_EAST, EAST_AFRICA );
+
+  link( URAL, UKRAINE );
+  link( URAL, AFGHANISTAN );
+  link( URAL, CHINA );
+  link( URAL, SIBERIA );
+
+  link( SIAM, INDIA );
+  link( SIAM, CHINA );
+  link( SIAM, INDONESIA );
+
+
+  link( EGYPT, THE_MIDDLE_EAST );
+  link( EGYPT, SOUTHERN_EUROPE );
+  link( EGYPT, NORTH_AFRICA );
+  link( EGYPT, EAST_AFRICA );
+
+  link( EAST_AFRICA, EGYPT );
+  link( EAST_AFRICA, THE_MIDDLE_EAST );
+  link( EAST_AFRICA, MADAGASCAR );
+  link( EAST_AFRICA, SOUTH_AFRICA );
+  link( EAST_AFRICA, CONGO );
+  link( EAST_AFRICA, NORTH_AFRICA );
+
+  link( MADAGASCAR, EAST_AFRICA );
+  link( MADAGASCAR, SOUTH_AFRICA );
+
+  link( SOUTH_AFRICA, MADAGASCAR );
+  link( SOUTH_AFRICA, EAST_AFRICA );
+  link( SOUTH_AFRICA, CONGO );
+
+  link( CONGO, NORTH_AFRICA );
+  link( CONGO, EAST_AFRICA );
+  link( CONGO, SOUTH_AFRICA );
+
+  link( NORTH_AFRICA, BRAZIL );
+  link( NORTH_AFRICA, WESTERN_EUROPE );
+  link( NORTH_AFRICA, SOUTHERN_EUROPE );
+  link( NORTH_AFRICA, EGYPT );
+  link( NORTH_AFRICA, EAST_AFRICA );
+  link( NORTH_AFRICA, CONGO );
+
+  link( INDONESIA, SIAM );
+  link( INDONESIA, NEW_GUINEA );
+  link( INDONESIA, WESTERN_AUSTRALIA );
+
+  link( NEW_GUINEA, INDONESIA );
+  link( NEW_GUINEA, WESTERN_AUSTRALIA );
+  link( NEW_GUINEA, EASTERN_AUSTRALIA );
+
+  link( WESTERN_AUSTRALIA, INDONESIA );
+  link( WESTERN_AUSTRALIA, NEW_GUINEA );
+  link( WESTERN_AUSTRALIA, EASTERN_AUSTRALIA );
+
+  link( EASTERN_AUSTRALIA, NEW_GUINEA );
+  link( EASTERN_AUSTRALIA, WESTERN_AUSTRALIA );
+
+  link( GREAT_BRITAIN, ICELAND );
+  link( GREAT_BRITAIN, SCANDANAVIA );
+  link( GREAT_BRITAIN, NORTHERN_EUROPE );
+  link( GREAT_BRITAIN, WESTERN_EUROPE );
+  
+  link( SCANDANAVIA, ICELAND );
+  link( SCANDANAVIA, GREAT_BRITAIN );
+  link( SCANDANAVIA, UKRAINE );
+  link( SCANDANAVIA, NORTHERN_EUROPE );
+
+  link( UKRAINE, SCANDANAVIA );
+  link( UKRAINE, NORTHERN_EUROPE );
+  link( UKRAINE, SOUTHERN_EUROPE );
+  link( UKRAINE, THE_MIDDLE_EAST );
+  link( UKRAINE, AFGHANISTAN );
+  link( UKRAINE, URAL );
+
+  link( NORTHERN_EUROPE, SCANDANAVIA );
+  link( NORTHERN_EUROPE, GREAT_BRITAIN );
+  link( NORTHERN_EUROPE, WESTERN_EUROPE );
+  link( NORTHERN_EUROPE, SOUTHERN_EUROPE );
+  link( NORTHERN_EUROPE, UKRAINE );
+
+
+  link( SOUTHERN_EUROPE, THE_MIDDLE_EAST );
+  link( SOUTHERN_EUROPE, UKRAINE );
+  link( SOUTHERN_EUROPE, NORTHERN_EUROPE );
+  link( SOUTHERN_EUROPE, WESTERN_EUROPE );
+  link( SOUTHERN_EUROPE, NORTH_AFRICA );
+  link( SOUTHERN_EUROPE, EGYPT );
+
+  link( WESTERN_EUROPE, GREAT_BRITAIN );
+  link( WESTERN_EUROPE, NORTHERN_EUROPE );
+  link( WESTERN_EUROPE, SOUTHERN_EUROPE );
+  link( WESTERN_EUROPE, NORTH_AFRICA );
+
+
 
   
 #ifdef DEBUG
   cerr << "done linking nodes" << endl;
 #endif
+
+  for( int i=0; i<nodes_vector.size(); i++ )
+    {
+      setWeight( i, rand()%6+2 );
+      //setWeight( i, 1 );
+    }
   
-  setWeight( 0, 0 );
-  setWeight( 1, 2 );
-  setWeight( 2, 3 );
-  setWeight( 3, 1 );
-  setWeight( 4, 1 );
-  setWeight( 5, 2 );
-  setWeight( 6, 2 );
-  setWeight( 7, 3 );
-  setWeight( 8, 3 );
-  setWeight( 9, 3 );
-  setWeight( 10, 2 );
-  setWeight( 11, 1 );
-  setWeight( 12, 5 );
-  setWeight( 13, 1 );
-  setWeight( 14, 1 );
 #ifdef DEBUG
   cerr << "done setting weights" << endl;
 #endif
   
-  getEasiestPath( nodes_vector[0], nodes_vector[12], 0 );
+  getEasiestPath( nodes_vector[ALASKA], nodes_vector[MADAGASCAR], 0 );
 #ifdef DEBUG  
   cout << "found " << goal_count << " solutions" << endl;
 #endif
 
   for( int i=0; i<path.size(); i++ )
      {
-       cout << *path[i];
-       if( i < path.size()-1) cout << "->";
+       cout << "[" << *path[i] << "]";
+       if( i < path.size()-1) cout << " -> ";
      }
    cout << endl;
   return 0;
